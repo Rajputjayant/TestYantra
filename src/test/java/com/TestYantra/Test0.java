3e360a1142ad2788e72075d131ca487c9fdbd993
DELL <DELL@DESKTOP-7SNL7GR>
2022-11-23 16:06:19 +0530

    Qsp

---
+++ src/test/java/com/TestYantra/Test0.java
@@ -0,0 +1,13 @@
+package com.TestYantra;
+
+import org.testng.annotations.Test;
+
+public class Test0 {
+
+	@Test
+    public void demo()
+    {
+		System.out.println("Mango");
+		System.out.println("Apple");
+    }
+}
